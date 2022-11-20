0f901b25c477e35aeea83e81fabfe49049b92c82
jimbrian <ui.ux.jimbrian@gmail.com>
2022-11-20 18:03:18 +0300

    rails resources create

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,11 @@
 class BirdsController < ApplicationController
 
+  # POST /birds
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
   # GET /birds
   def index
     birds = Bird.all
@@ -8,6 +14,8 @@ class BirdsController < ApplicationController
 
   # GET /birds/:id
   def show
+    # params[:id] refers to the dynamic part of our route, defined by :id
+    # a request to /birds/2 would give params[:id] a value of 2
     bird = Bird.find_by(id: params[:id])
     if bird
       render json: bird
